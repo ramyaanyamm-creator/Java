46689336b90fb1603ef5684f6608e1e2a8b8652a
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2025-12-14 21:28:20 +0530

    Add files via upload

--- palindrome.java
+++ palindrome.java
@@ -1,14 +1,15 @@
 class Palindrome {
     public static void main(String[] args) {
-        int n = 121, temp = n, rev = 0;
+        int num = 121;
+        int original = num;
+        int rev = 0;
 
-        while(temp != 0) {
-            int digit = temp % 10;
-            rev = rev * 10 + digit;
-            temp /= 10;
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
         }
 
-        if(n == rev)
+        if (original == rev)
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");

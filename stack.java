3cbbe5683468a930c843951ed8866c206d114a0b
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2025-12-19 19:20:11 +0530

    Add files via upload

---
+++ stack.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+             System.out.println(isBalanced(input));
+            //Complete the code
+		}
+        sc.close();
+		
+	}
+     private static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+
+

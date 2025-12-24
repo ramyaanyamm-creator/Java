d4dbf3137114d0bd4b3a89ec0939edab937d3ffb
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2025-12-24 19:22:30 +0530

    Add files via upload

---
+++ palindromatic.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public String longestPalindrome(String s) {
+        String longest = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            String odd = checkPalindrome(s, i, i);
+            String even = checkPalindrome(s, i, i + 1);
+
+            if (odd.length() > longest.length()) {
+                longest = odd;
+            }
+
+            if (even.length() > longest.length()) {
+                longest = even;
+            }
+        }
+
+        return longest;
+    }
+    private String checkPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+}
+s

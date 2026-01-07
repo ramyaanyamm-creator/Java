0753a2a1647f497b2b0221eaad8a3a739c4770d8
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2026-01-07 20:20:18 +0530

    Add files via upload

---
+++ kgroup.java
@@ -0,0 +1,35 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode current = dummy, next = dummy, prev = dummy;
+
+        // Count nodes
+        int count = 0;
+        while (current.next != null) {
+            current = current.next;
+            count++;
+        }
+
+        // Reverse in groups of k
+        while (count >= k) {
+            current = prev.next;
+            next = current.next;
+
+            for (int i = 1; i < k; i++) {
+                current.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+                next = current.next;
+            }
+
+            prev = current;
+            count -= k;
+        }
+
+        return dummy.next;
+    }
+}

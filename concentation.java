26974fee6ac5eda3d8276a510f79e1e6a4c10e92
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2026-01-13 18:00:48 +0530

    Add files via upload

---
+++ concentation.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || words == null || words.length == 0) return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        if (s.length() < totalLen) return result;
+
+        // Frequency map of words
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String w : words) {
+            wordMap.put(w, wordMap.getOrDefault(w, 0) + 1);
+        }
+
+        // Try each possible offset
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, count = 0;
+            Map<String, Integer> windowMap = new HashMap<>();
+
+            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
+                String word = s.substring(right, right + wordLen);
+
+                if (wordMap.containsKey(word)) {
+                    windowMap.put(word, windowMap.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    // If word appears too many times, shrink window
+                    while (windowMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    // Valid window found
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                } else {
+                    // Reset window
+                    windowMap.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+}
